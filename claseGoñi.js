e553f2a8c488e8af37228056fb36ed52d3fae42f
Picoquio <84530694+Picoquio@users.noreply.github.com>
2021-11-23 01:13:15 -0300

    agregué semicolons

--- claseGoñi.js
+++ claseGoñi.js
@@ -4,11 +4,11 @@ let number = prompt('Introduzca una tecla cualquiera');
 
 // Flujo:
 if (number.length == 1) {
-    alert('Su indice de masa corporal se encuentra dentro de los estándares saludables.')
+    alert('Su indice de masa corporal se encuentra dentro de los estándares saludables.');
 }
 else if (number.length > 1) {
-    alert('Los dedos que usó para marcar son demasiado gordos. Para obtener una barra especial, oprima todos los botones con la palma de su mano.')
+    alert('Los dedos que usó para marcar son demasiado gordos. Para obtener una barra especial, oprima todos los botones con la palma de su mano.');
 }
 else {
-    alert('Error: debe ingresar al menos un caracter. Por favor cargue nuevamente esta página.')
+    alert('Error: debe ingresar al menos un caracter. Por favor cargue nuevamente esta página.');
 }
